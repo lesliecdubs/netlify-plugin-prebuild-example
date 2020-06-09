b96e711b030aaea50c7e496895b5737726ed994c
lesliecdubs <leslie@netlify.com>
2020-06-09 18:07:40 -0500

    Break a test on purpose

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress" />
-describe('Gatsby blog', () => {
+describe("Gatsby blog", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit("/")
   })
 
-  it('loads home page', () => {
-    cy.get('main > h1')
+  it("loads home page", () => {
+    cy.get("main > h1").should("include", "Hellllllo")
   })
 
-  it('navigates to second page and back', () => {
-    cy.contains('a', 'Go to page 2').click()
-    cy.url().should('match', /page-2/)
-    cy.contains('main > h1', 'Hi from the second page')
-    cy.contains('a', 'Go back to the homepage').click()
-    cy.url().should('include', Cypress.config('baseUrl'))
+  it("navigates to second page and back", () => {
+    cy.contains("a", "Go to page 2").click()
+    cy.url().should("match", /page-2/)
+    cy.contains("main > h1", "Hi from the second page")
+    cy.contains("a", "Go back to the homepage").click()
+    cy.url().should("include", Cypress.config("baseUrl"))
   })
 })
